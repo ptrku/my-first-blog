6fe145ac64ba14bdccbf7e3c6b145440137afb5f
ptrku <patrycjusz.krogul@gmail.com>
2020-04-25 20:15:13 +0200

    First commit

---
+++ python_intro.py
@@ -0,0 +1,11 @@
+def hi(imie):
+    print('Hej ' + imie + '!')
+
+dziewczyny = ['Ania', 'Basia', 'Kasia', 'Asia']
+
+for imie in dziewczyny:
+    hi(imie)
+    print('kolejna')
+
+for i in range(4, 8):
+    print(i)
